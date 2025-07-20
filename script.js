7244fba82fc0f12cb93f2575ff6b23563c0b2988
Tekalign-T7 <tekaligntekola@gmail.com>
2025-07-20 14:55:21 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const container=document.querySelector("#container");
+const button=document.querySelector("#button");
+const reset=document.querySelector(".reset-button");
+reset.addEventListener("click",()=>{
+ location.reload();
+})
+button.addEventListener("click",()=>{
+    let info=prompt("choose the number of boxes Between 20-100 😁and Start Drawing✌️Enjoy!") **2;
+    let number=0;
+    while (number<=info){
+        const NewDiv=document.createElement('div');
+        NewDiv.classList.add("box");
+        container.appendChild(NewDiv);
+        NewDiv.textContent=" ";
+        NewDiv.addEventListener('mouseover', () => {
+        const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        NewDiv.style.backgroundColor = randomColor;});
+        number=number+1;
+    }
+        
+});
+
+
